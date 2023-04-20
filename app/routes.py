e47c211b368b35c8aac629b5c39e68f9e58cfea5
Angela <echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"')  /Users/an5073/.zprofile>
2023-04-20 11:41:26 -0400

    Wave 1 started

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id = None, name = None, description = None, moons = None):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons or []
+
+mercury = Planet(self, id, "Mercury", description, moons)
