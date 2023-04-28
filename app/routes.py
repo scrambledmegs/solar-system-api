481fae79cfd291be6b47dae4cf0489a52ad89f6d
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-04-28 12:10:36 -0400

    commented out list of planets

--- app/routes.py
+++ app/routes.py
@@ -10,16 +10,16 @@ import uuid
 #         self.name = name
 #         self.description = description
 
-planets = [
-    Planet(None, "Mercury", "Fastest and smallest planet, slightly larger than Earth\'s moon."),
-    Planet(None, "Venus", "Hottest planet that spins in opposite direction from most planets."),
-    Planet(None, "Earth", "Only planet inhabited by living things and with liquid on the surface."),
-    Planet(None, "Mars", "Dusty, cold, desert world. Mars used to be wetter and warmer billions of years ago."),
-    Planet(None, "Jupiter", "Twice the size of other planets. The Great Red spot is a centuries-old storm larger than Earth."),
-    Planet(None, "Saturn", "Complex system of dazzling, icy rings."),
-    Planet(None, "Uranus", "Rotates at a nearly 90 degree angle making it appear to spin on its side."),
-    Planet(None, "Neptune", "Most distant from the sun. Neptune is dark, cold, and whipped by supersonic winds. First planet located through mathematical calculations.")
-]
+# planets = [
+#     Planet(None, "Mercury", "Fastest and smallest planet, slightly larger than Earth\'s moon."),
+#     Planet(None, "Venus", "Hottest planet that spins in opposite direction from most planets."),
+#     Planet(None, "Earth", "Only planet inhabited by living things and with liquid on the surface."),
+#     Planet(None, "Mars", "Dusty, cold, desert world. Mars used to be wetter and warmer billions of years ago."),
+#     Planet(None, "Jupiter", "Twice the size of other planets. The Great Red spot is a centuries-old storm larger than Earth."),
+#     Planet(None, "Saturn", "Complex system of dazzling, icy rings."),
+#     Planet(None, "Uranus", "Rotates at a nearly 90 degree angle making it appear to spin on its side."),
+#     Planet(None, "Neptune", "Most distant from the sun. Neptune is dark, cold, and whipped by supersonic winds. First planet located through mathematical calculations.")
+# ]
 
 # def validate_planet(planet_id):
 #     try:
@@ -42,7 +42,7 @@ def create_planet():
         name = request_body["name"],
         description = request_body["description"]
     )
-    
+
 
     db.session.add(new_planet)
     db.session.commit()
