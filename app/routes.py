987c67439389514ac059da948e15d48e09d4219a
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-03 11:27:07 -0400

    adds read one planet, update planet, delete planet

--- app/routes.py
+++ app/routes.py
@@ -21,15 +21,18 @@ import uuid
 #     Planet(None, "Neptune", "Most distant from the sun. Neptune is dark, cold, and whipped by supersonic winds. First planet located through mathematical calculations.")
 # ]
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         abort(make_response({"message": f"Planet id is invalid. {planet_id} is {type(planet_id)}"}, 400))
-#     for planet in planets:
-#         if planet_id == planet.id:
-#             return planet
-#     abort(make_response({"message":f"Planet id {planet_id} not found"}, 404))
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"Planet id is invalid. {planet_id} is {type(planet_id)}"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"Planet id {planet_id} not found"}, 404))
+    
+    return planet
 
 
 planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
@@ -51,11 +54,29 @@ def create_planet():
         f'Planet {new_planet.name} successfully created', 201
     )
 
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f'Planet #{planet.id} sucessfully updated', 200)
+
+
 @planets_bp.route("", methods = ["GET"])
 def retrieve_planet():
-    planets_response = []
-    planets = Planet.query.all()
+    name_query = request.args.get("name")
+    if name_query:
+        planets = Planet.query.filter_by(name = name_query)
+    else:
+        planets = Planet.query.all()
 
+    planets_response = []
     for planet in planets:
         planets_response.append({
             "id": planet.id,
@@ -65,22 +86,21 @@ def retrieve_planet():
 
     return jsonify(planets_response, 200)
 
-# @planets_bp.route("", methods = ["GET"])
-# def handle_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append({
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#         }), 200
-#     return jsonify(planets_response)
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def read_planet(planet_id):
+    planet = validate_planet(planet_id)
 
-# @planets_bp.route("/<planet_id>", methods = ["GET"])
-# def handle_planet(planet_id):
-#     planet = validate_planet(planet_id)
-#     return {
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#     }, 200
+    return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+    }
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully deleted")
